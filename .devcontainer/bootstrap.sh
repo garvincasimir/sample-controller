11d56b92e47d3df60a8935f06cea5abff9859f3c
Garvin Casimir <garvincasimir@users.noreply.github.com>
2023-03-21 11:10:09 -0400

    Add dev container

---
+++ .devcontainer/bootstrap.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+
+#Install KUBECTL
+if ! command -v kubectl &> /dev/null
+then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+fi
+
+#Setup Docker Registry
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
+
+# KIND
+if ! command -v kind &> /dev/null
+then
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+    chmod +x ./kind
+    sudo mv ./kind /usr/local/bin/kind
+fi
+
+# Skaffold
+if ! command -v skaffold &> /dev/null
+then
+    curl -fsSL -o skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+    chmod +x ./skaffold
+    sudo mv ./skaffold /usr/local/bin/
+fi
+
+if kind get clusters  2>&1 | grep -q "No kind clusters"; then
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:5000"]
+nodes:
+- role: control-plane
+- role: worker
+EOF
+fi
+
+
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+
+if id -u vagrant &>/dev/null; then
+    if [ ! -d "/home/vagrant/.kube" ]; then
+        cp -r ~/.kube /home/vagrant
+        chown -R vagrant:vagrant /home/vagrant
+    fi
+fi
+
+kubectl get nodes
